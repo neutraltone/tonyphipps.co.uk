c17acdfa36d46eeeef90584e689d5ec316196237
Tony Phipps <tonyphipps@gmail.com>
2015-10-14 12:02:42 +0100

    Add: Contact form markup Add: Contact form reveal

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,8 +3,29 @@
   'use strict';
 
   $(function () {
+
+    $('.js--contact-form-trigger').on('click', function(e){
+      e.preventDefault();
+
+      var $main          = $('.main');
+      var $sidebar       = $('.sidebar');
+      var $sidebarHeight = $sidebar.outerHeight();
+
+      console.log($sidebarHeight);
+
+      $main.animate({
+        top: '-' + $sidebarHeight,
+      }, 300 );
+    });
+
+    /**
+     * Window Load Calls
+     */
     $(window).load(function() {
+
+      // Remove Loading Overlay
       $('.loading').fadeOut();
+
     });
   });
 
