08d0dc1e4eeab676882b7a0a4c1e7c0f1ef286a1
Tony Phipps <tonyphipps@gmail.com>
2015-10-30 20:41:02 +0000

    Mod: Configure mailer.php

--- mailer.php
+++ mailer.php
@@ -1,11 +1,9 @@
 <?php
 if($_POST)
 {
-    // Dev
-    $to_email       = "tony@gritdigital.com"; //Recipient email, Replace with own email here
 
-    // Live
-    //$to_email       = "tony@gritdigital.com"; //Recipient email, Replace with own email here
+    //Recipient email, Replace with own email here
+    $to_email       = "tony@tonyphipps.co.uk";
 
     //check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
@@ -18,18 +16,16 @@ if($_POST)
     }
 
     //Sanitize input data using PHP filter_var().
-    $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
-    $company        = filter_var($_POST["comapny"], FILTER_SANITIZE_STRING);
-    $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
-    $phone_number   = filter_var($_POST["telephone"], FILTER_SANITIZE_NUMBER_INT);
-    $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+    $name      = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email     = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message   = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
 
     //additional php validation
-    if(strlen($user_name)<4){ // If length is less than 4 it will output JSON error.
+    if(strlen($name)<4){ // If length is less than 4 it will output JSON error.
         $output = json_encode(array('type'=>'error', 'text' => 'Your name is too short or empty.'));
         die($output);
     }
-    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ //email validation
         $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email address.'));
         die($output);
     }
@@ -43,14 +39,14 @@ if($_POST)
     }
 
     //email subject
-    $subject = "EastPort Website Enquiry from ".$user_name;
+    $subject = "[ Website Enquiry ] from: ".$name;
 
     //email body
-    $message_body = "Name: ".$user_name."\r\nEmail: ".$user_email."\r\nPhone Number: ". $phone_number."\r\n\r\n".$message ;
+    $message_body = "Name: ".$name."\r\nEmail: ".$email."\r\n\r\n".$message ;
 
     //proceed with PHP email.
-    $headers = 'From: '.$user_name.'' . "\r\n" .
-    'Reply-To: '.$user_email.'' . "\r\n" .
+    $headers = 'From: '.$name.'' . "\r\n" .
+    'Reply-To: '.$email.'' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
     $send_mail = mail($to_email, $subject, $message_body, $headers);
@@ -61,7 +57,7 @@ if($_POST)
         $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
         die($output);
     }else{
-        $output = json_encode(array('type'=>'message', 'text' => '<span>'.$user_name .'</span>. Thank you for your email. We will be in touch shortly.'));
+        $output = json_encode(array('type'=>'message', 'text' => '<span>'.$name .'</span>. Thank you for your email. We will be in touch shortly.'));
         die($output);
     }
 }
