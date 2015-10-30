8a163b1b3786cf0ca831e51b215fe398403e7533
Tony Phipps <tonyphipps@gmail.com>
2015-10-30 22:47:39 +0000

    Mod: Tweaks to the contact form validation

--- app/mailer.php
+++ app/mailer.php
@@ -3,7 +3,7 @@ if($_POST)
 {
 
     //Recipient email, Replace with own email here
-    $to_email       = "tony@tonyphipps.co.uk";
+    $to_email       = "tonyphipps@gmail.com";
 
     //check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
@@ -18,7 +18,7 @@ if($_POST)
     //Sanitize input data using PHP filter_var().
     $name      = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
     $email     = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $message   = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+    $message   = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
     //additional php validation
     if(strlen($name)<4){ // If length is less than 4 it will output JSON error.
@@ -27,10 +27,6 @@ if($_POST)
     }
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ //email validation
         $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email address.'));
-        die($output);
-    }
-    if(!filter_var($phone_number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
-        $output = json_encode(array('type'=>'error', 'text' => 'Only digits allowed in phone number.'));
         die($output);
     }
     if(strlen($message)<3){ //check emtpy message
@@ -45,7 +41,7 @@ if($_POST)
     $message_body = "Name: ".$name."\r\nEmail: ".$email."\r\n\r\n".$message ;
 
     //proceed with PHP email.
-    $headers = 'From: '.$name.'' . "\r\n" .
+    $headers = 'From: '.$email.'' . "\r\n" .
     'Reply-To: '.$email.'' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
@@ -57,7 +53,7 @@ if($_POST)
         $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
         die($output);
     }else{
-        $output = json_encode(array('type'=>'message', 'text' => '<span>'.$name .'</span>. Thank you for your email. We will be in touch shortly.'));
+        $output = json_encode(array('type'=>'message', 'text' => '<span>Hi '.$name .'</span>, Thank you for getting in touch. I have recieved your email and will get back to you shortly.'));
         die($output);
     }
 }
